6adc42ff85d81e2f01091426601757f335581d50
Sirjana <sirjanabhatta6@gmail.com>
2024-07-15 20:21:57 +0545

    checkpont added

--- scripts/finetune/vit_b_k710_ft.sh
+++ scripts/finetune/vit_b_k710_ft.sh
@@ -7,7 +7,7 @@ OUTPUT_DIR='/kaggle/working/videomae/checkpoints'
 DATA_PATH='/kaggle/input/cricshots-csvfiles/csv_cricshot'
 # DATA_PATH='/kaggle/input/csv-files-for-cricshot/crichotcsv'
 
-MODEL_PATH='/kaggle/input/vitbase/vit_b_k710_dl_from_giant.pth'
+# MODEL_PATH='/kaggle/input/vitbase/vit_b_k710_dl_from_giant.pth'
 MODEL_PATH='/kaggle/input/videomae-epoch17/checkpoint-17.pth'
 
 JOB_NAME=$1
