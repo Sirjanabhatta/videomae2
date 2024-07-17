f9c8df25f3ca931bb2058a17a7ea9950a1602cdc
Sirjana <sirjanabhatta6@gmail.com>
2024-07-17 10:24:01 +0545

    3 classs

--- engine_for_finetuning.py
+++ engine_for_finetuning.py
@@ -198,13 +198,12 @@ def validation_one_epoch(data_loader, model, device):
         
 
         
-        # acc1, acc5 = accuracy(output, target, topk=(1, 5))
-        acc1 = accuracy(output, target, topk=(1,))[0]
+        acc1, acc5 = accuracy(output, target, topk=(1, 5))
 
         batch_size = images.shape[0]
         metric_logger.update(loss=loss.item())
         metric_logger.meters['acc1'].update(acc1.item(), n=batch_size)
-        # metric_logger.meters['acc5'].update(acc5.item(), n=batch_size)
+        metric_logger.meters['acc5'].update(acc5.item(), n=batch_size)
     # gather the stats from all processes
     metric_logger.synchronize_between_processes()
     print(
